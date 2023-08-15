9903e0a6ca2fc89ff2dae9ca1e5721d5480bb6d5
shinevue <shine401020@gmail.com>
2023-08-15 13:52:26 +0200

    removed redundant Url class, introduced recogizing endcoding

--- pwright.py
+++ pwright.py
@@ -20,7 +20,7 @@ URL = 'https://www.biznes.gov.pl/pl'
 
 class PlayScraper:
     """
-    Main worker class for the scraper using Playwright
+    Main outer class for the scraper using Playwright
     """
     playwright = sync_playwright().start()
     browser = None
@@ -32,7 +32,7 @@ class PlayScraper:
     def __init__(self, url: str, render_javascript: bool = False):
         self.render_javascript = render_javascript
         self.metrics = None
-        self.url_obj = self.Url(url)
+        self.page_content = self.PageContent(url)
 
     def create_browser(self):
         """ Create browser and context instances """
@@ -45,20 +45,20 @@ class PlayScraper:
         page = self.context.new_page()
         # cdp_session = self.context.new_cdp_session(page)
         try:
-            self.response: Response = page.goto(self.url_obj.url)
+            self.response: Response = page.goto(self.page_content.url)
         except TimeoutError:
             self.response_error = "Connection Timeout"
         except Error as e:
             print(e.message)
 
         if self.response is not None:
-            self.url_obj.status = self.response.status
-            self.url_obj.headers = self.response.headers
-            self.url_obj.body = self.response.body()
+            self.page_content.status = self.response.status
+            self.page_content.headers = self.response.headers
+            self.page_content.body = self.response.body()
             try:
-                self.url_obj.text = self.response.text()
+                self.page_content.raw_html = self.response.text()
             except UnicodeDecodeError:
-                self.url_obj.text = self.response.body().decode("utf-8", errors="ignore")
+                self.page_content.text = self.response.body().decode("utf-8", errors="ignore")
 
         # if self.render_javascript:
         #     self.metrics = cdp_session.send("Performance.getMetrics")
@@ -68,13 +68,34 @@ class PlayScraper:
     def run(self):
         self.create_browser()
         self.visit_page()
-        self.url_obj.set_url_content()
-        self.url_obj.page_content.build_content()
+        self.page_content.build_content()
+
+    # @dataclass
+    # class Url:
+    #     """
+    #     Class representing URL metadata & raw, unprocessed data
+    #     """
+    #     url: str
+    #     status: Optional[str | int] = None
+    #     headers: Optional[dict] = None
+    #     body: Optional[str] = None
+    #     text: Optional[str] = None
+    #
+    #     def __init__(self, url):
+    #         self.url = url
+    #         self.page_content = self.PageContent()
+    #
+    #     def set_url_content(self):
+    #         self.page_content.raw_html = self.text
+    #
+    #     def get_html_encoding(self):
+    #         content_type = self.headers['content-type'].split(';')
+    #         self.page_content.encoding = content_type[1].strip().split('=').strip()
 
     @dataclass
-    class Url:
+    class PageContent:
         """
-        Class representing URL metadata & raw, unprocessed data
+        Class representing actual content of the page build from its response
         """
         url: str
         status: Optional[str | int] = None
@@ -82,52 +103,44 @@ class PlayScraper:
         body: Optional[str] = None
         text: Optional[str] = None
 
-        def __init__(self, url):
-            self.url = url
-            self.page_content = self.PageContent()
+        title: Optional[str] = None
+        raw_html: Optional[str] = None
+        html_tree: Optional[ElementTree] = None
 
-        def set_url_content(self):
-            self.page_content.raw_html = self.text
+        def build_content(self):
+            self.normalize_html()
+            self.build_html_tree()
+            self.get_title()
 
-        @dataclass
-        class PageContent:
-            """
-            Class representing actual content of the page build from its response
-            """
-            title: Optional[str] = None
-            page_encoding: Optional[str] = None
-            raw_html: Optional[str] = None
-            html_tree: Optional[ElementTree] = None
+        @property
+        def encoding(self):
+            content_type = self.headers['content-type'].split(';')
+            encoding = content_type[1].strip().split('=')[1].strip()
+            return 'latin1' if encoding == 'latin-1' else encoding
 
-            def build_content(self):
-                self.normalize_html()
-                self.build_html_tree()
-                self.get_title()
+        def normalize_html(self):
+            if self.raw_html:
+                self.raw_html = unicodedata.normalize("NFKC", unquote(self.raw_html.strip()))  # encoding=self.page_encoding
 
-            def normalize_html(self):
-                if self.raw_html:
-                    self.raw_html = unicodedata.normalize("NFKC", unquote(self.raw_html.strip()))  # encoding=self.page_encoding
+        def build_html_tree(self):
+            temp_bytes = self.raw_html.encode(self.encoding, errors='backslashreplace')
 
-            def build_html_tree(self):
-                encoding = "latin1"
-                temp_bytes = self.raw_html.encode(encoding, errors='backslashreplace')
+            tree_parser = html.HTMLParser(remove_comments=True, recover=True)
+            self.html_tree = lxml.html.fromstring(temp_bytes, parser=tree_parser)
 
-                tree_parser = html.HTMLParser(remove_comments=True, recover=True)
-                self.html_tree = lxml.html.fromstring(temp_bytes, parser=tree_parser)
+        def get_title(self):
+            if len(self.html_tree.xpath("//title[1]//text()")) > 0:
+                self.title = " ".join(unquote(str(self.html_tree.xpath("//title[1]//text()")[0])).split()).strip()
 
-            def get_title(self):
-                if len(self.html_tree.xpath("//title[1]//text()")) > 0:
-                    self.title = " ".join(unquote(str(self.html_tree.xpath("//title[1]//text()")[0])).split()).strip()
+        def count_h1(self):
+            pass
 
-            def count_h1(self):
-                pass
-
-            def count_imgs(self):
-                pass
+        def count_imgs(self):
+            pass
 
 
 if __name__ == '__main__':
     url1 = PlayScraper(url=URL, render_javascript=False)
     url1.run()
-    print(url1.url_obj.page_content.title)
-
+    print(url1.page_content.headers)
+    print(url1.page_content.title)
