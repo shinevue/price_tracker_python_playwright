175c4f269257f539d552d835bc52695fe8985a27
shinevue <shine401020@gmail.com>
2024-03-27 19:22:37 +0100

    extract me product data method

--- src/base/product_base.py
+++ src/base/product_base.py
@@ -2,7 +2,7 @@ class Product:
     def __init__(
             self,
             name: str | None = None,
-            price: int | None = None
+            price: int | float | None = None
         ) -> None:
         self.name = name
         self.price = price
--- src/me/page_me.py
+++ src/me/page_me.py
@@ -1,6 +1,9 @@
 from src.base.page_base import CategoryPage, ProductPage
 from src.base.product_base import Product
+from src.exceptions import UnmatchingPrices
+from src.me.selectors_me import DomainDataME
 
+from src import utils
 
 class MECategoryPage(CategoryPage):
     def extract_products_urls(self):
@@ -12,4 +15,12 @@ class MECategoryPage(CategoryPage):
 class MEProductPage(ProductPage):
     def extract_product_data(self) -> Product:
         product_item = Product()
+        product_item.name = self.page_content.html_tree.xpath(DomainDataME.XPathSelectors['product_name'])[0]
+        prices_found = self.page_content.html_tree.xpath(DomainDataME.XPathSelectors['main_price'])
+        if prices_found:
+            if utils.compare_list_elements(prices_found):
+                product_item.price = float(prices_found[0][:-2] + '.' + prices_found[0][-2:])
+            else:
+                print('prices_found: ', prices_found)
+                raise UnmatchingPrices
         return product_item
