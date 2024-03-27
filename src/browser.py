74d07e33595d0c7150fa9b1c5e15e967653a698e
shinevue <shine401020@gmail.com>
2024-03-27 18:26:09 +0100

    browser & page classes separated

--- src/browser.py
+++ src/browser.py
@@ -1,37 +1,6 @@
-import unicodedata
-
-from typing import Optional
-from urllib.parse import unquote
-import lxml.html
-import lxml.etree
 from playwright.sync_api import sync_playwright, Response, TimeoutError
 
-
-class PageContent:
-    def __init__(
-        self, url: str, status_code: int, headers: dict, body: bytes, raw_html: str
-    ):
-        self.url = url
-        self.status_code = status_code
-        self.headers = headers
-        self.body = body
-        self.raw_html = raw_html
-
-    @property
-    def encoding(self) -> Optional[str]:
-        content_type = self.headers["content-type"].split(";")
-        try:
-            encoding = content_type[1].strip().split("=")[1].strip()
-            return "latin1" if encoding == "latin-1" else encoding
-        except IndexError:
-            return None
-
-    @property
-    def html_tree(self) -> lxml.etree.ElementTree:
-        tree_parser = lxml.html.HTMLParser(remove_comments=True, recover=True)
-        normalized_html = unicodedata.normalize("NFKC", unquote(self.raw_html.strip()))
-        return lxml.html.fromstring(normalized_html, parser=tree_parser)
-
+from src.page import PageContent
 
 
 class Browser:
