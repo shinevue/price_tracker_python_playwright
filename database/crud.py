81cbc265195aece22790d2f005c7c1951130394a
shinevue <shine401020@gmail.com>
2024-04-25 19:39:40 +0200

    add base crud

---
+++ database/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import insert, select, update, delete
+
+
+class CRUD:
+    def __init__(self, model):
+        self.model = model
+
+    def create(self, db: Session, obj_in):
+        db.execute(insert(self.model).values(obj_in))
+        db.commit()
+
+    def read(self, db: Session, id):
+        return db.execute(select(self.model).where(self.model.id == id)).first()
+
+    def update(self, db: Session, id, obj_in):
+        db.execute(update(self.model).where(self.model.id == id).values(obj_in))
+        db.commit()
+
+    def delete(self, db: Session, id):
+        db.execute(delete(self.model).where(self.model.id == id))
+        db.commit()
